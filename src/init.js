8d855886d9fbbe3e87f0ecaaa9a009a2948a2b7b
Dmitriy Frolov <omgoole@ya.ru>
2021-01-21 00:35:41 +0500

    started

--- src/init.js
+++ src/init.js
@@ -1,3 +1,44 @@
+import axios from 'axios';
+import onChange from 'on-change';
+import 'bootstrap';
+// import $ from 'jquery';
+import { parseRSS } from './utils.js';
+
+const render = (state) => {
+
+};
+
 export default () => {
-  console.log('init successful');
+  const state = {
+    feeds: [],
+    posts: [],
+    error: null,
+  };
+
+  const watchedState = onChange(state, () => render(watchedState));
+
+  // https://ru.hexlet.io/lessons.rss
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('#url_input');
+    axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${input.value}`)
+      .then((res) => { console.log(res); return res; })
+      .then((res) => parseRSS(res.data.contents))
+      .then(({ title, description, items }) => {
+        watchedState.feeds.push({ title, description });
+        watchedState.posts.push(...items);
+      })
+      .catch((err) => console.log(err));
+  });
+
+  // .then((parsed) => {
+  //   console.log(parsed.firstChild.tagName);
+  // });
+  // console.log($('#modal').modal);
+  // $('#modal').on('show.bs.modal', (event) => {
+  //   const button = $(event.relatedTarget);
+  //   const description = button.data('description');
+  //   $('.modal-body div').html(description);
+  // });
 };
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,19 @@
 // eslint-disable-next-line import/prefer-default-export
 export const parseRSS = (xmltext) => {
+  if (!xmltext) {
+    throw new Error('EMPTY_RSS_DATA');
+  }
   const doc = new DOMParser().parseFromString(xmltext, 'application/xml');
+
   const title = doc.querySelector('title').textContent;
   const description = doc.querySelector('description').textContent;
+
   const items = [...doc.querySelectorAll('item')].map((item) => {
     const itemTitle = item.querySelector('title').textContent;
     const link = item.querySelector('link').textContent;
     const itemDescription = item.querySelector('description').textContent;
     const guid = item.querySelector('guid').textContent;
+
     return {
       guid,
       title: itemTitle,
@@ -15,7 +21,6 @@ export const parseRSS = (xmltext) => {
       description: itemDescription,
     };
   });
-  console.log({ title, description, items });
-  console.log(doc);
+
   return { title, description, items };
 };
