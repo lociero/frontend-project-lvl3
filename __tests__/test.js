4c6c1d69441bc4af968f983d3db62fb95706a7e6
Dmitriy Frolov <omgoole@ya.ru>
2021-10-07 17:40:55 +0500

    test returned

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,38 +1,34 @@
-// import { screen } from '@testing-library/react';
-// import '@testing-library/jest-dom';
-// import { promises as fs } from 'fs';
-// import path from 'path';
-// import userEvent from '@testing-library/user-event';
-// import init from '../src/init.js';
+import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import { promises as fs } from 'fs';
+import path from 'path';
+import userEvent from '@testing-library/user-event';
+import init from '../src/init.js';
 
-// beforeEach(async () => {
-//   const pathToHtml = path.join(__dirname, '__fixtures__/index.html');
-//   const html = await fs.readFile(pathToHtml, 'utf-8');
-//   document.body.innerHTML = html;
-//   await init();
-// });
+beforeEach(async () => {
+  const pathToHtml = path.join(__dirname, '__fixtures__/index.html');
+  const html = await fs.readFile(pathToHtml, 'utf-8');
+  document.body.innerHTML = html;
+  await init();
+});
 
-// test('form init', () => {
-//   expect(screen.getByTestId('form_test')).toBeInTheDocument();
-// });
+test('form init', () => {
+  expect(screen.getByTestId('form_test')).toBeInTheDocument();
+});
 
-// test('input init', () => {
-//   expect(screen.getByRole('textbox', { name: 'url' })).toBeInTheDocument();
-// });
+test('input init', () => {
+  expect(screen.getByRole('textbox', { name: 'url' })).toBeInTheDocument();
+});
 
-// test('validation (unique)', async () => {
-//   const rssUrl = 'https://ru.hexlet.io/lessons.rss';
-//   userEvent.type(screen.getByRole('textbox', { name: 'url' }), rssUrl);
-//   userEvent.click(screen.getByRole('button', { name: 'add' }));
+test('validation (unique)', async () => {
+  const rssUrl = 'https://ru.hexlet.io/lessons.rss';
+  userEvent.type(screen.getByRole('textbox', { name: 'url' }), rssUrl);
+  userEvent.click(screen.getByRole('button', { name: 'add' }));
 
-//   expect(await screen.findByText(/RSS успешно загружен/i)).toBeInTheDocument();
+  expect(await screen.findByText(/RSS успешно загружен/i)).toBeInTheDocument();
 
-//   userEvent.type(screen.getByRole('textbox', { name: 'url' }), rssUrl);
-//   userEvent.click(screen.getByRole('button', { name: 'add' }));
+  userEvent.type(screen.getByRole('textbox', { name: 'url' }), rssUrl);
+  userEvent.click(screen.getByRole('button', { name: 'add' }));
 
-//   expect(await screen.findByText(/RSS уже существует/i)).toBeInTheDocument();
-// });
-
-test('fun', () => {
-  expect(null).toBeNull();
+  expect(await screen.findByText(/RSS уже существует/i)).toBeInTheDocument();
 });
